1497c5ed613c21b021970101841b2fb4652d95a2
Adhurim Canhasi <adhurimc@gmail.com>
2025-04-29 21:42:23 +0200

    dependencies fix

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true, // Ensure this is enabled for App Router
-  },
-};
-
-export default nextConfig;
