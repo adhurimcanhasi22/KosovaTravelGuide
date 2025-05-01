bb519d94e19e541bd1d5d20e545f8c94bf5cd8da
Adhurim Canhasi <adhurimc@gmail.com>
2025-05-01 17:09:35 +0200

    server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+require('./config/db');
+const express = require('express');
+const cors = require('cors');
+const UserRouter = require('./api/User');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// CORS middleware should come FIRST
+app.use(
+  cors({
+    origin: [
+      'https://kosovatravelguide.netlify.app/', // Frontend
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+);
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Routes
+app.use('/user', UserRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
