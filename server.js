3d32d4f8a6dc2ff828a838042634552609b7b346
Adhurim Canhasi <adhurimc@gmail.com>
2025-05-01 17:08:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const UserRouter = require('./api/User');
 
 const app = express();
-const port = proccess.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 // CORS middleware should come FIRST
 app.use(
