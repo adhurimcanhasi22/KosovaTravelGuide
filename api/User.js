b438aad6715f2e366f0930612602f5949c16d6aa
Adhurim Canhasi <adhurimc@gmail.com>
2025-05-03 17:09:04 +0200

    route protection

--- api/User.js
+++ api/User.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
-import { protect } from '../middleware/authMiddleware';
-
+const { protect } = require('../middleware/authMiddleware');
 //mongodb user model
 const User = require('./../models/User');
 
